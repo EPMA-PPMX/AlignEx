fc4c78860574c53842a72bc9ec92cff34175b3b9
ameypusalkar001 <Amey.pusalkar@epmainc.com>
2025-12-01 22:45:11 +0530

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import ChatbaseWidget from './ChatbaseWidget';
-
+ 
 interface LayoutProps {
   children: React.ReactNode;
 }
-
+ 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-50 flex-col">
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 overflow-auto">
-          {children}
-        </main>
-      </div>
-      <footer className="bg-white border-t border-gray-200 py-3 px-6 text-center text-sm text-gray-600">
-        EPMA All Rights Reserved © 2025
-      </footer>
-    <div className="flex h-screen bg-gray-50">
-      <Sidebar />
-      <main className="flex-1 overflow-auto">
+<div className="flex h-screen bg-gray-50">
+<Sidebar />
+<main className="flex-1 overflow-auto">
         {children}
-      </main>
-      <ChatbaseWidget />
-    </div>
+</main>
+<ChatbaseWidget />
+</div>
   );
 };
-
+ 
 export default Layout;
