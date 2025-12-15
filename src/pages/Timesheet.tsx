cff3e1d77462ffb9f965809c7c560924375b5b9f
parthivbhuta <73834958+parthivbhuta@users.noreply.github.com>
2025-12-15 11:35:52 -0600

    Updated Timesheet.tsx

--- src/pages/Timesheet.tsx
+++ src/pages/Timesheet.tsx
@@ -245,6 +245,10 @@ const Timesheet: React.FC = () => {
     setRows(allRows);
   }, [currentWeekStart]);
 
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   const getWeekDates = () => {
     const dates = [];
     for (let i = 0; i < 7; i++) {
