2a66d2d661747ab98127c8eb5f6954344e522e63
ameypusalkar001 <Amey.pusalkar@epmainc.com>
2026-01-10 23:20:26 +0530

    Add files via upload

--- src/components/widgets/TimesheetQuickWidget.tsx
+++ src/components/widgets/TimesheetQuickWidget.tsx
@@ -22,6 +22,7 @@ export default function TimesheetQuickWidget() {
 
   useEffect(() => {
     fetchThisWeekHours();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchThisWeekHours = async () => {
