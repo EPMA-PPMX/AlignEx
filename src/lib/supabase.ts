67b1c3d5612396447d016935e6210b837ddadea6
ameypusalkar001 <Amey.pusalkar@epmainc.com>
2026-07-02 18:27:58 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { from } from './api-client';
-const supabaseUrl='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYmFydnhpbHFubndqbWRxeG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2Njk3NDMsImV4cCI6MjA3NzI0NTc0M30.ZXgtWx6qRZ4UChWxlf6CRODlEUTki5p-EGmTpsBfX1Q'
-const supabaseAnonKey='https://qhbarvxilqnnwjmdqxog.supabase.co'
+const supabaseAnonKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYmFydnhpbHFubndqbWRxeG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2Njk3NDMsImV4cCI6MjA3NzI0NTc0M30.ZXgtWx6qRZ4UChWxlf6CRODlEUTki5p-EGmTpsBfX1Q'
+const supabaseUrl='https://qhbarvxilqnnwjmdqxog.supabase.co'
 
 //const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 //const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
